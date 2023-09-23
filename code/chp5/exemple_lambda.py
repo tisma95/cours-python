8a6a2dd4a1c66386be320e2eaafc1931c05accd0
Ismaël Maurice <ismael.tuo@edigrp.com>
2023-09-23 11:54:47 +0200

    feat(chp5): rédaction fonction lambda
    Ajout de la section sur les fonctions lambda

---
+++ code/chp5/exemple_lambda.py
@@ -0,0 +1,17 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+>>> carre = lambda x: x * x
+>>> carre(2)
+4
+>>> carre(3)
+9
+>>> # Equivalent avec def
+>>> def carre2(x):
+...     return x * x
+... 
+>>> carre2(2)
+4
+>>> carre2(3)
+9
+>>> 
