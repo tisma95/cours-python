67706597eef3067b8fa4b217b0d46f2532efd7b9
Ismaël Maurice <ismael.tuo@edigrp.com>
2023-10-14 15:58:06 +0200

    Feat(chp6): Ajout des imports et exercices
    Rédaction de la méthode d'important from module import ...
    
    Rédaction des exercices
    
    Export du ours en pdf

---
+++ code/chp6/example_from_import.py
@@ -0,0 +1,101 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+# Importer uniquement la fonction de puissance
+from math import pow
+help(pow)
+Help on built-in function pow in module math:
+
+pow(x, y, /)
+    Return x**y (x to the power of y).
+
+pow(2, 2)
+4.0
+
+# Importer les fonctions valeur absolue abs et puissance pow du module mathématique
+from math import pow, abs
+Traceback (most recent call last):
+  File "<pyshell#6>", line 1, in <module>
+    from math import pow, abs
+ImportError: cannot import name 'abs' from 'math' (unknown location)
+# Importer les fonctions valeur absolue abs et puissance pow du module mathématique
+from math import pow, fabs# Importer les fonctions valeur absolue abs et puissance pow du module mathématique
+
+
+# Importer les fonctions valeur absolue fabs et puissance pow du module mathématique
+from math import pow, fabs
+help(fabs)
+Help on built-in function fabs in module math:
+
+fabs(x, /)
+    Return the absolute value of the float x.
+
+help(math)
+Traceback (most recent call last):
+  File "<pyshell#14>", line 1, in <module>
+    help(math)
+NameError: name 'math' is not defined
+# Importer les fonctions valeur absolue fabs et puissance pow du module mathématique
+
+
+
+
+
+
+# Importer les fonctions valeur absolue fabs et puissance pow du module mathématique
+from math import pow, fabs
+help(fabs)
+Help on built-in function fabs in module math:
+
+fabs(x, /)
+    Return the absolute value of the float x.
+
+help(pow)
+Help on built-in function pow in module math:
+
+pow(x, y, /)
+    Return x**y (x to the power of y).
+
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> # Importer tous les fonctions du module mathématiques
+>>> from math import *
+>>> pow(2,2)
+4.0
+>>> help(fabs)
+Help on built-in function fabs in module math:
+
+fabs(x, /)
+    Return the absolute value of the float x.
+
+>>> 
+>>> 
+>>> 
+
+... 
+>>> 
+
+>>> 
+
+... 
+>>> 
+
+>>> 
+
+... 
+>>> 
+
+>>> 
+
+... 
