3f98ffd505d3bda4f360491155dae3fea4be063a
Ismaël Maurice <ismael.tuo@edigrp.com>
2024-07-28 09:11:48 +0200

    Feat(chp4) Correction de l'exercice 6

--- exercices_et_corrections/chapitre_4/exo6.py
+++ exercices_et_corrections/chapitre_4/exo6.py
@@ -10,4 +10,11 @@
     ce nombre (inclus). Par exemple, si l’utilisateur entre 5, le programme doit calculer 1 + 2 + 3 + 4 + 5 = 15
 """
 
-# TODO: Ecrire le code en python ci dessous
+nbreSaisi = input("Entrer un nombre:")
+nbreSaisi = int(nbreSaisi)
+
+# Affichage du total uniquement
+total = 0
+for nbreCompteur in range(1, nbreSaisi+1):
+    total = total + nbreCompteur
+print(f"La somme des nombres de 1 à {nbreSaisi} est: {total}")
