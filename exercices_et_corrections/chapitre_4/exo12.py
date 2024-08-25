ead9f4923f297e67520a1cb5d6945bceeb11bd3e
Ismaël Maurice <ismael.tuo@edigrp.com>
2024-08-25 08:22:35 +0200

    Feat(chp4): Revue exercice 12

--- exercices_et_corrections/chapitre_4/exo12.py
+++ exercices_et_corrections/chapitre_4/exo12.py
@@ -11,38 +11,30 @@
     Exemple l’utilisateur entre « TOTO » ou « toTO » le programme doit afficher « Toto ».
 """
 
-# TODO: Ecrire le code en python ci dessous
-
-#Methode chapitre 4
-print("\nMethode TOF seul:")
-phraseMot = input("Entrez votre phrase ou votre mot: ")
+# Méthode avec la boucle While
+print("\nMéthode utilisant la boucle while:\n")
+phraseMot = input("Entrez une phrase ou une mot: ")
 totalCaracteres = len(phraseMot)
 index = 0
 while index < totalCaracteres:
     caractereActuel = phraseMot[index]
     if index == 0:
-        print(caractereActuel.upper(),end="")
+        print(caractereActuel.upper(), end="")
     else:
-        print(caractereActuel.lower(),end="")
+        print(caractereActuel.lower(), end="")
     index = index + 1
 
-#Methode chapitre 4 TEST TOF
-print("\n\nMethode TEST TOF:") # \n\n pour sauter une ligne du fait que le programme TOF seul ai un end=""
-phraseMot = input("Entrez votre phrase ou votre mot en mélangeant MAJUSCULES et minuscules:\n-> ")
-totalCaracteres = len(phraseMot)
-index = 0
-print("\nUne phrase commence par une majuscule et fini par un point.\nVoici votre phrase corrigée:\n-> ",end="")
-while index < totalCaracteres:
-    caractereActuel = phraseMot[index]
+# Méthode avec la boucle For
+print("\n\nMéthode utilisant la boucle For:\n")
+phraseMot = input("Entrez une phrase ou une mot: ")
+for index, lettre in enumerate(phraseMot):
     if index == 0:
-        print(caractereActuel.upper(),end="")
+        print(lettre.upper(), end="")
     else:
-        print(caractereActuel.lower(),end="")
-    index = index + 1
-print(".")
+        print(lettre.lower(), end="")
 
-#Methode avec l'aide du chapitre 8 
-print("\nMethode chapitre 8: (Les méthodes de bases de la classe str)")
+# Méthode avec l'aide du chapitre 8
+print("\n\nMéthode utilisant les fonctions de la classe str:\n")
 saisieUtilisateur = input("Veuillez entrer une phrase ou un mot:")
 saisieUtilisateur = saisieUtilisateur.capitalize()
-print(saisieUtilisateur,end=".")
+print(saisieUtilisateur)
