3ab03c019fd764afca30f8f0159d21ba006b53ed
tof29400 <76088856+tof29400@users.noreply.github.com>
2024-08-25 09:15:29 +0200

    tof exo 14

--- exercices_et_corrections/chapitre_4/exo14.py
+++ exercices_et_corrections/chapitre_4/exo14.py
@@ -12,3 +12,27 @@
 """
 
 # TODO: Ecrire le code en python ci dessous
+
+# Définition des variables
+S_MAX = 10
+maxNombre = None
+positionNbre = 1
+
+# Démarrage du programme
+print("+++++++++++++ SAISIE DES 20 NOMBRES +++++++++++++++++++\n")
+for ind in range(S_MAX):
+    saisieUtilisateur = input(f"Entrer le nombre numéro {ind + 1}:")
+        # On suppose que l'utilisateur a entré un nombre
+    saisieUtilisateur = int(saisieUtilisateur)
+    # Mise à jour de la valeur de max
+    if maxNombre == None:
+        print(f"Initialisation de la valeur max à {saisieUtilisateur}.")
+        maxNombre = saisieUtilisateur
+    elif saisieUtilisateur > maxNombre:
+        print(f"Changement de la valeur max qui passe de {maxNombre} à {saisieUtilisateur}")
+        maxNombre = saisieUtilisateur
+        positionNbre = positionNbre + 1
+     # # Méthode rapide
+    # if maxNombre == None or saisieUtilisateur > maxNombre:
+    #     maxNombre = saisieUtilisateur
+print(f"\nLe plus grand de ces nombres est {maxNombre} et c'est le nombre {positionNbre} saisi.\n")
