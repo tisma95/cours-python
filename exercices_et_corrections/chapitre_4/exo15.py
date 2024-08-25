ddd2c63e3fc4c63213d5cf260d848af5fdaeaa60
tof29400 <76088856+tof29400@users.noreply.github.com>
2024-08-25 09:55:11 +0200

    exo 15 tof

--- exercices_et_corrections/chapitre_4/exo15.py
+++ exercices_et_corrections/chapitre_4/exo15.py
@@ -13,3 +13,30 @@
 """
 
 # TODO: Ecrire le code en python ci dessous
+
+# Définition des variables + choix du nombre d'entrées par l utilisateur
+S_MAX = input("\nVeuillez choisir la quantité de nombres à saisir: ")
+S_MAX = int(S_MAX)
+if S_MAX <=0:
+    print("Saisie Incorecte")
+else:
+    maxNombre = None
+    positionNbre = 1
+    # Démarrage du programme
+    print(f"+++++++++++++ SAISIE DES {S_MAX} NOMBRES +++++++++++++++++++\n")
+    for ind in range(S_MAX):
+        saisieUtilisateur = input(f"Entrer le nombre numéro {ind + 1}: ")
+        # On suppose que l'utilisateur a entré un nombre
+        saisieUtilisateur = int(saisieUtilisateur)
+    # Mise à jour de la valeur de max
+        if maxNombre == None:
+            print(f"Initialisation de la valeur max à {saisieUtilisateur}.")
+            maxNombre = saisieUtilisateur
+        elif saisieUtilisateur > maxNombre:
+            print(f"Changement de la valeur max qui passe de {maxNombre} à {saisieUtilisateur}")
+            maxNombre = saisieUtilisateur
+            positionNbre = positionNbre + 1
+     # # Méthode rapide
+    # if maxNombre == None or saisieUtilisateur > maxNombre:
+    #     maxNombre = saisieUtilisateur
+print(f"\nLe plus grand de ces nombres est {maxNombre} et c'est le nombre {positionNbre} saisi.\n")
